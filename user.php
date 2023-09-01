6401ea87e2776ac89bf502ee2b6402906a3e66ed
TullioTambone <tulliotambone97@gmail.com>
2023-09-01 17:17:13 +0200

    il db viene popolato

--- user.php
+++ user.php
@@ -1,10 +1,33 @@
 <?php
+include __DIR__ . "/connect.php";
+
 $pageTitle = "CRUD - user Page";
 include __DIR__ . "/layout/top_html.php";
+
+
+// se vi sono dati da inviare
+if(isset($_POST['submit'])){
+    //setto i dati da inviare
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+
+    $query = " insert into crud (name, email, mobile, password)
+    values('$name', '$email', '$mobile', '$password')";
+
+    $result = mysqli_query($connection,$query);
+
+    if($result){
+        echo 'data inserted successfully';
+    }else{
+        die(mysqli_error($connection));
+    }
+}
 ?>
 
 <div class="container py-5">
-    <form class="mb-3" method="POST">
+    <form class="mb-3" method="post">
     
         <label for="name" class="form-label">Name</label>
         <input type="text" class="form-control" name="name" placeholder="enter your name">
@@ -13,12 +36,12 @@ include __DIR__ . "/layout/top_html.php";
         <input type="email" class="form-control" name="email" placeholder="abc@mail.com">
 
         <label for="mobile" class="form-label mt-3">Mobile</label>
-        <input type="text" class="form-control" name="name" placeholder="enter your number">
+        <input type="text" class="form-control" name="mobile" placeholder="enter your number">
 
         <label for="password" class="form-label mt-3">Password</label>
         <input type="text" class="form-control" name="password" placeholder="password">
 
-        <button type="submit" class="btn btn-primary mt-5"><a href="" class="text-light">SUBMIT</a></button>
+        <button type="submit" class="btn btn-primary mt-5" name="submit">SUBMIT</button>
     </form>
 </div>
 
